c2d39ff9a398764e468ccab9b3f57d8f7fc87dab
Nate <mijaresmacmac08@gmail.com>
2024-12-25 21:47:16 +0800

    added Icon pictures

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-const mybutton = document.getElementById("gig");
+  const mybutton = document.getElementById("gig");
 
-window.onscroll = function() {scrollings()};
+  window.onscroll = function() {scrollings()};
 
-function scrollings() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
+  function scrollings() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
   }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
